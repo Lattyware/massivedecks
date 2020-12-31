933d0760fb4a2bd79749a3459f3aa16cd3a0eecd
Gareth Latty <gareth@lattyware.co.uk>
2020-12-31 01:23:28 +0000

    Update build process a little.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,8 +5,8 @@ const path = require("path");
 const CompressionPlugin = require("compression-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const sass = require("sass");
-const ClosurePlugin = require("closure-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const fs = require("fs");
 
 module.exports = (env, argv) => {
   const mode =
@@ -16,7 +16,7 @@ module.exports = (env, argv) => {
       ? process.env["WEBPACK_MODE"]
       : "production";
 
-  const prod = mode === "production";
+  const production = mode === "production";
 
   const dist = path.resolve(__dirname, "dist");
   const src = path.resolve(__dirname, "src");
@@ -54,118 +54,36 @@ module.exports = (env, argv) => {
     }
   }
 
-  const cssLoaders = [
-    // Load CSS to inline styles.
-    {
-      loader: "css-loader",
-      options: { sourceMap: !prod },
-    },
-    // Transform CSS for compatibility.
-    {
-      loader: "postcss-loader",
-      options: {
-        sourceMap: !prod,
-      },
-    },
-    // Allow relative URLs.
-    {
-      loader: "resolve-url-loader",
-      options: { sourceMap: !prod },
-    },
-    // Load SASS to CSS.
-    {
-      loader: "sass-loader",
-      options: {
-        implementation: sass,
-        sourceMap: true,
-        sassOptions: {
-          includePaths: ["node_modules"],
-        },
-      },
-    },
-  ];
-
-  const elmLoaders = [
-    {
-      loader: "elm-webpack-loader",
-      options: {
-        optimize: prod,
-        debug: !prod,
-      },
-    },
-  ];
-
-  const plugins = [
-    new InjectMetadataPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "src/html/index.html",
-      filename: "index.html",
-      inject: "body",
-      excludeChunks: ["cast"],
-      test: /\.html$/,
-    }),
-    new HtmlWebpackPlugin({
-      template: "src/html/cast.html",
-      filename: "cast.html",
-      inject: "body",
-      excludeChunks: ["index"],
-      test: /\.html$/,
-    }),
-  ];
-
-  if (prod) {
-    cssLoaders.unshift({
-      loader: MiniCssExtractPlugin.loader,
-    });
-    plugins.push(
-      new CompressionPlugin({
-        test: /\.(js|css|html|webmanifest|svg)$/,
-      }),
-      new CompressionPlugin({
-        test: /\.(js|css|html|webmanifest|svg)$/,
-        filename: "[path].br[query]",
-        algorithm: "brotliCompress",
-        compressionOptions: { level: 11 }
-      })
-    );
-    plugins.push(new webpack.HashedModuleIdsPlugin());
-  } else {
-    cssLoaders.unshift({ loader: "style-loader" });
-    // Load CSS without refreshing in a dev env.
-    cssLoaders.unshift({
-      loader: "css-hot-loader",
-      options: {
-        reloadAll: true,
-      },
-    });
-    // Load elm without refreshing in a dev env.
-    // Disable if working with chromecasts.
-    // noinspection JSCheckFunctionSignatures
-    elmLoaders.unshift({ loader: "elm-hot-webpack-loader" });
-    plugins.push(new webpack.HotModuleReplacementPlugin());
-  }
-
   return {
     context: path.resolve(__dirname),
     entry: {
       // Main entry point.
-      index: "./src/ts/index.ts",
+      index: path.join(src, "ts", "index.ts"),
       // Chromecast entry point.
-      cast: "./src/ts/cast.ts",
+      cast: path.join(src, "ts", "cast.ts"),
     },
     output: {
       path: dist,
       publicPath: "/",
       filename: "assets/scripts/[name].[contenthash].js",
     },
+    mode,
     module: {
       rules: [
         // Elm scripts.
         {
           test: /\.elm$/,
           exclude: [/elm-stuff/, /node_modules/],
-          use: elmLoaders,
+          use: [
+            ...(production ? [] : [{ loader: "elm-hot-webpack-loader" }]),
+            {
+              loader: "elm-webpack-loader",
+              options: {
+                optimize: production,
+                debug: !production,
+              },
+            },
+          ],
         },
         // Typescript scripts.
         {
@@ -178,7 +96,41 @@ module.exports = (env, argv) => {
         {
           test: /\.s?css$/,
           exclude: [/elm-stuff/, /node_modules/],
-          use: cssLoaders,
+          use: [
+            production
+              ? {
+                  loader: MiniCssExtractPlugin.loader,
+                }
+              : { loader: "style-loader" },
+            // Load CSS to inline styles.
+            {
+              loader: "css-loader",
+              options: { sourceMap: !production },
+            },
+            // Transform CSS for compatibility.
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: !production,
+              },
+            },
+            // Allow relative URLs.
+            {
+              loader: "resolve-url-loader",
+              options: { sourceMap: !production },
+            },
+            // Load SASS to CSS.
+            {
+              loader: "sass-loader",
+              options: {
+                implementation: sass,
+                sourceMap: true,
+                sassOptions: {
+                  includePaths: ["node_modules"],
+                },
+              },
+            },
+          ],
         },
         // Image assets.
         {
@@ -223,21 +175,34 @@ module.exports = (env, argv) => {
       extensions: [".js", ".ts", ".elm", ".scss"],
       modules: ["node_modules"],
     },
-    plugins: plugins,
+    plugins: [
+      new InjectMetadataPlugin(),
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "assets/styles/[name].[contenthash].css",
+      }),
+      new HtmlWebpackPlugin({
+        template: path.join(src, "html", "index.html"),
+        filename: "index.html",
+        inject: "body",
+        excludeChunks: ["cast"],
+      }),
+      new HtmlWebpackPlugin({
+        template: path.join(src, "html", "cast.html"),
+        filename: "cast.html",
+        inject: "body",
+        excludeChunks: ["index"],
+      }),
+      ...(production
+        ? [
+            new CompressionPlugin({
+              test: /\.(js|css|html|webmanifest|svg)$/,
+            }),
+          ]
+        : [new webpack.HotModuleReplacementPlugin()]),
+    ],
     optimization: {
       minimizer: [
-        new ClosurePlugin(
-          {
-            mode: "STANDARD",
-            platform: "java",
-            test: /assets\/scripts\/.*\.js$/,
-          },
-          {
-            compilation_level: "SIMPLE_OPTIMIZATIONS",
-            externs: "src/js/extern.js",
-            languageOut: "ECMASCRIPT6_STRICT",
-          }
-        ),
         new TerserPlugin({
           test: /assets\/scripts\/.*\.js$/,
           parallel: true,
@@ -249,7 +214,7 @@ module.exports = (env, argv) => {
         }),
       ],
     },
-    devtool: !prod ? "eval-source-map" : undefined,
+    devtool: !production ? "eval-source-map" : undefined,
     devServer: {
       hot: true,
       allowedHosts: ["localhost"],
